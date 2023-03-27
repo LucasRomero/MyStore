d6d34b3566968fa6c3d2ff3b7d5e60a9e8b2cd89
LucasRomero <romerolucasdaniel@outlook.com>
2023-03-27 14:34:47 -0300

    Building cart page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+// Angular Material
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatMenuModule } from "@angular/material/menu";
@@ -8,23 +9,33 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { MatExpansionModule } from "@angular/material/expansion";
-import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTableModule } from "@angular/material/table";
 import { MatBadgeModule } from "@angular/material/badge";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatListModule } from "@angular/material/list";
 
+// Components
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./pages/home/home.component";
-import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
-import { FiltersComponent } from './pages/home/components/filters/filters.component';
-import { ProductBoxComponent } from './pages/components/product-box/product-box.component';
+import { ProductsHeaderComponent } from "./pages/home/components/products-header/products-header.component";
+import { FiltersComponent } from "./pages/home/components/filters/filters.component";
+import { ProductBoxComponent } from "./pages/components/product-box/product-box.component";
+import { CartComponent } from "./pages/cart/cart.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent, ProductBoxComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    ProductBoxComponent,
+    CartComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -36,11 +47,11 @@ import { ProductBoxComponent } from './pages/components/product-box/product-box.
     MatCardModule,
     MatIconModule,
     MatExpansionModule,
-    MatListModule,
     MatToolbarModule,
     MatTableModule,
     MatBadgeModule,
     MatSnackBarModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
